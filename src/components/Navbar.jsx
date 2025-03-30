22ef803fabf722d56035e8bcdcfa0707505e773e
Vincent Cancelliere <vcancell@stevens.edu>
2025-03-30 07:37:32 -0400

    Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,11 @@ import DuckProfile from "/DuckProfile.png"
 const Navbar = () => {
   return(
       <div className="bg-blue-500 text-white p-4 flex justify-between">
+        <a href="/">
+          <img src="/BetterBills.png" alt="Better Bills Logo" className="h-15 w-fit" />
+        </a>
         <div className="absolute top-2 right-4">
-        <ProfileIcon icon={<img src={DuckProfile} className="w-12 h-12 rounded-full" />} />
+        <ProfileIcon icon={<img src={DuckProfile} className="w-15 h-15 rounded-full" />} />
         </div>
       </div>
   );
